222ac839da00b2b9dee50132b7cfd73c56c95002
your_username <udaysingh.pawar@student.griffith.ie>
2023-03-31 22:37:24 +0100

    Implemented scanner object for user input and added infinite while for continuous input of city names

--- SD2_Chatbot_Nootnoot/src/main/java/Code_Review_1_Nootnoot.java
+++ SD2_Chatbot_Nootnoot/src/main/java/Code_Review_1_Nootnoot.java
@@ -6,7 +6,24 @@ public class Code_Review_1_Nootnoot {
 	public static void main(String[] args) throws Exception {
     	String open_weather_api_key = "b369057d518ed3e182c04c76c1ec73fe"; //This is the OpenWeatherMap API key
     	Scanner scan = new Scanner(System.in); //Created a scanner object
-    	System.out.println("Hello");//Nootnoot saying hello
+    	System.out.println("Heya");//Nootnoot saying hello
+    	System.out.println("Welcome to the Weather ChatBot! \nHi my name's Nootnoot"); //Nootnoot introduces itself and prints a welcome message to the user
+
+        while (true) { //Inifite loop until user types 'quit' or 'q'
+        	System.out.println("What location would you like to know the weather for? "); //Printing users input
+        	String city = scan.nextLine(); //Reading users input
+
+        	//This code represnts how the user can end the conversation with Nootnoot
+        	//This code functionality lets the user too quit the Nootnoot chatbot via "quit" or "q" commands
+        	if (city.equalsIgnoreCase("quit")) {
+                System.out.println("Thank you and have a great day :)");
+                break;
+            }
+            else if (city.equalsIgnoreCase("q")) {
+                System.out.println("Thanks you and have a great day :)");
+                break;
+            }
+        }
 
     	//Implementing try-catch block
     	try {
@@ -15,7 +32,6 @@ public class Code_Review_1_Nootnoot {
     	catch(Exception e) {
     		//  Block of code to handle errors
     	}
-    }
 
+	}
 }
-
